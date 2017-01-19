6daa5593cc80c387432a211460f0a5f1aa8b9b64
Mircea Ionica <mionica@gmail.com>
2017-01-19 19:29:13 +0000

    update gdk-pixbuf to 2.34.0

--- ts/ports/opt/gdk-pixbuf/gdk-pixbuf-register.sh
+++ ts/ports/opt/gdk-pixbuf/gdk-pixbuf-register.sh
@@ -3,6 +3,6 @@
 # /usr/bin/gtk-register: register gdk-pixbuf loaders
 #
 
-/usr/bin/gdk-pixbuf-query-loaders > /usr/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache
+gdk-pixbuf-query-loaders --update-cache
 
 # End of file
